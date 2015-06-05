b35fa8605d430850949d79925b338d3b5bea3ba3
David D. Riddle <ddriddle@illinois.edu>
2015-06-05 16:09:18 -0500

    Add support for apache and SCL Collections
    The project is now able to run from the service directory, with the
    supplied apache config file and the necessary modifications needed
    for scls.

--- main/wsgi.py
+++ main/wsgi.py
@@ -8,6 +8,15 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+from sdg.scl import SDGCollections
+
+SDGCollections.enable('python27', 'sdg_2015a_python27')
+
+this_dir = os.path.dirname(__file__)
+BASE_DIR = os.path.abspath(os.path.join(this_dir, '..'))
+sys.path.insert(0, BASE_DIR)
 
 from django.core.wsgi import get_wsgi_application
 
--- manage.py
+++ manage.py
@@ -3,6 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
+    BASE_DIR = os.path.dirname(__file__)
+    sys.path.insert(0, BASE_DIR)
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 
     from django.core.management import execute_from_command_line
